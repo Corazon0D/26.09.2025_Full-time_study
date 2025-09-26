7b4061f6690e806d26e93509b621c95ae01499b4
Corazon0D <korazon@gmail.com>
2025-09-26 16:29:38 +0300

    wowoww

--- Bridge.py
+++ Bridge.py
@@ -1,2 +1,15 @@
+
 h_bus = 437
-m = print('введи количество мостов') # количество Мостов
+m = int(input('введи количество мостов: ')) # количество Мостов
+
+count = 0
+for i in range(m) :
+    d = int(input('высоту мостов: '))
+    if h_bus < d:
+        count += 1
+
+    if h_bus >= d:
+        print(f'Crash {i + 1}')
+        break
+if h_bus < d:
+    print('No chash')
